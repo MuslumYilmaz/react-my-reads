5745bb7b8934ff760aa9f3ed230ef9eb28022abf
MuslumYilmaz <muslum@vizyon.ai>
2021-07-16 12:27:37 +0300

    fetch values display

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   {this.state.books.map((book, i) => {
@@ -82,10 +81,69 @@ class BooksApp extends React.Component {
                       </li>
                     </ol>
                   </div>
-                    )
+                  )
                   })}
                 </div>
-              </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  {this.state.books.map((book, i) => {
+                    console.log(book);
+                    return book.shelf === "wantToRead" && (
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li key={i}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    </ol>
+                  </div>
+                  )
+                  })}
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  {this.state.books.map((book, i) => {
+                    console.log(book);
+                    return book.shelf === "read" && (
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li key={i}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    </ol>
+                  </div>
+                  )
+                  })}
+                </div>
             </div>
             <div className="open-search">
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
