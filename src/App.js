446c1972cdf55ede7302366c991bb40c5d14785e
MuslumYilmaz <muslum@vizyon.ai>
2021-07-16 12:03:13 +0300

    book map array

--- src/App.js
+++ src/App.js
@@ -57,12 +57,15 @@ class BooksApp extends React.Component {
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
+                  {this.state.books.map((book, i) => {
+                    console.log(book);
+                    return book.shelf === "currentlyReading" && (
+                    <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
+                      <li key={i}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -73,12 +76,14 @@ class BooksApp extends React.Component {
                               </select>
                             </div>
                           </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
                         </div>
                       </li>
                     </ol>
                   </div>
+                    )
+                  })}
                 </div>
               </div>
             </div>
