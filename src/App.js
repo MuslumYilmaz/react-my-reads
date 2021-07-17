4abec6c5fe1cba62a276104bade336e61c951beb
MuslumYilmaz <muslum@vizyon.ai>
2021-07-17 17:24:28 +0300

    book shelf select option for searched books

--- src/App.js
+++ src/App.js
@@ -72,11 +72,20 @@ class BooksApp extends React.Component {
                     <ol className="books-grid">
                       <li>
                         <div className="book">
-                        { book.imageLinks !== undefined && (
                           <div className="book-top">
+                        { book.imageLinks !== undefined && (
                               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        )}
+                        <div className="book-shelf-changer">
+                              <select value={book.shelf} onChange={(e) => this.handleChange(book, e)}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
                           </div>
-                          )}
                           <div className="book-title">{book.title}</div>
                           <div className="book-authors">{book.authors}</div>
                         </div>
